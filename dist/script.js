612eda34bc0fcc523be3268c05141452bd451caa
Olena Horal <ohoral@cogniance.com>
2018-01-10 19:26:00 +0200

    Meeting rabbito

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,44 @@
 'use strict';
 
 window.onload = function () {
+  var body = document.querySelector('body');
   var runningBunny = document.getElementById('runningBunny');
   var waitingBunny = document.getElementById('waitingBunny');
+  var hearts = document.getElementById('hearts');
   var Observable = Rx.Observable;
   var mdown = Observable.fromEvent(runningBunny, 'mousedown');
   var mup = Observable.fromEvent(document, 'mouseup');
   var mmove = Observable.fromEvent(document, 'mousemove');
+  // const menter = Observable.fromEvent(waitingBunny, 'mouseenter');
+  // const mleave = Observable.fromEvent(waitingBunny, 'mouseleave');
 
-  var drag = mdown.map(function (event) {
+  var meeting = function meeting() {
+    var waitingCoordinates = waitingBunny.getBoundingClientRect();
+    var runningCoordinates = runningBunny.getBoundingClientRect();
+
+    return runningCoordinates.top > waitingCoordinates.top && runningCoordinates.top < waitingCoordinates.bottom && runningCoordinates.left > waitingCoordinates.left && runningCoordinates.left < waitingCoordinates.right || runningCoordinates.bottom > waitingCoordinates.top && runningCoordinates.bottom < waitingCoordinates.bottom && runningCoordinates.right > waitingCoordinates.left && runningCoordinates.right < waitingCoordinates.right;
+  };
+
+  var run = mdown.map(function (event) {
     return mmove.takeUntil(mup);
   }).concatAll();
 
-  drag.subscribe(function (event) {
+  // menter.subscribe(() => {
+  //   console.log("in")
+  //   body.classList.add("hearts");
+  // });
+  //
+  // mleave.subscribe(() => {
+  //   console.log("out")
+  //   body.classList.remove("hearts");
+  // });
+
+  run.subscribe(function (event) {
+    if (meeting() && !body.classList.contains('hearts')) {
+      body.classList.add("hearts");
+    } else if (!meeting() && body.classList.contains('hearts')) {
+      body.classList.remove("hearts");
+    }
     runningBunny.style.left = event.clientX + 'px';
     runningBunny.style.top = event.clientY + 'px';
   });
--- src/script.js
+++ src/script.js
@@ -1,19 +1,49 @@
-window.onload = ()=>{
+window.onload = () => {
+  const body = document.querySelector('body');
   const runningBunny = document.getElementById('runningBunny');
   const waitingBunny = document.getElementById('waitingBunny');
+  const hearts = document.getElementById('hearts');
   const Observable = Rx.Observable;
   const mdown = Observable.fromEvent(runningBunny, 'mousedown');
   const mup = Observable.fromEvent(document, 'mouseup');
   const mmove = Observable.fromEvent(document, 'mousemove');
-  const mmove = Observable.fromEvent(waitingBunny, 'mousemove');
+  // const menter = Observable.fromEvent(waitingBunny, 'mouseenter');
+  // const mleave = Observable.fromEvent(waitingBunny, 'mouseleave');
 
-  const drag = mdown.map((event) => {
+  const meeting = () => {
+    const waitingCoordinates = waitingBunny.getBoundingClientRect();
+    const runningCoordinates = runningBunny.getBoundingClientRect();
+
+    return (
+        runningCoordinates.top > waitingCoordinates.top&&runningCoordinates.top < waitingCoordinates.bottom
+        &&runningCoordinates.left > waitingCoordinates.left&&runningCoordinates.left < waitingCoordinates.right
+        ||runningCoordinates.bottom > waitingCoordinates.top&&runningCoordinates.bottom < waitingCoordinates.bottom
+        &&runningCoordinates.right > waitingCoordinates.left&&runningCoordinates.right < waitingCoordinates.right
+    )
+  };
+
+  const run = mdown.map((event) => {
         return mmove.takeUntil(mup)
       }
   ).concatAll();
 
-  drag.subscribe((event) => {
+  // menter.subscribe(() => {
+  //   console.log("in")
+  //   body.classList.add("hearts");
+  // });
+  //
+  // mleave.subscribe(() => {
+  //   console.log("out")
+  //   body.classList.remove("hearts");
+  // });
+
+  run.subscribe((event) => {
+    if (meeting()&& !body.classList.contains('hearts')) {
+      body.classList.add("hearts");
+    } else if (!meeting() && body.classList.contains('hearts')) {
+      body.classList.remove("hearts");
+    }
     runningBunny.style.left = event.clientX + 'px';
     runningBunny.style.top = event.clientY + 'px';
-  })
+  });
 };
